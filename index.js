8c38b6f9f81bcc501aaa8673e487cdb088f25f02
Sebastian <seb154@live.com>
2023-11-26 11:53:10 -0500

    modifies index.js and package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    // Target the paragraph element with id="text"
+    var paragraph = document.getElementById('text');
+
+    // Replace the text content with "This is really cool!"
+    paragraph.textContent = "This is really cool!";
+});
